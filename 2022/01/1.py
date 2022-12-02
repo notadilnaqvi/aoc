35a9f005acf756c1229a4504b97e66900ffd1891
Adil Naqvi <notadilnaqvi@gmail.com>
2022-12-02 14:48:46 +0500

    01: elves and calories

---
+++ 2022/01/1.py
@@ -0,0 +1,13 @@
+with open('01-input.txt') as f:
+    content = f.read()
+    elves = content.split('\n\n')
+    calories_per_elf = []
+    for elf in elves:
+      calories = elf.split('\n')
+      calories_of_current_elf = 0
+      for calorie in calories:
+        calories_of_current_elf += int(calorie)
+      calories_per_elf.append(calories_of_current_elf)
+
+    calories_per_elf_sorted = sorted(calories_per_elf, reverse=True)
+    print(calories_per_elf_sorted[0])
