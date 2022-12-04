3e53bb86b42cd8550a06434fad301b236eb7aa55
Adil Naqvi <notadilnaqvi@gmail.com>
2022-12-04 16:32:55 +0500

    04: ranges are VERY hard - improved range overlap calculation logic

--- 2022/04/2.py
+++ 2022/04/2.py
@@ -21,7 +21,11 @@ def solve(file):
         elf_1_range = determine_sections_range(elf_1)
         elf_2_range = determine_sections_range(elf_2)
 
-        ranges_have_overlap = elf_1_range.get('start') <= elf_2_range.get('end') and elf_1_range.get('end') >= elf_2_range.get('start') 
+        # Instead of checking if the ranges overlap, we check if they don't overlap and invert the result
+        # There are only 2 ways for range 1 to NOT overlap with range 2.
+        # 1. Range 1 is completely to the left of range 2 (i.e. range 2's start is greater than range 1's end) ------- [range 1] [range 2]
+        # 2. Range 1 is completely to the right of range 2 (i.e. range 2's end is less than range 1's start) --------- [range 2] [range 1]
+        ranges_have_overlap = not (elf_2_range.get('start') > elf_1_range.get('end') or elf_2_range.get('end') < elf_1_range.get('start'))
 
         if(ranges_have_overlap):
             number_of_pairs_that_have_any_overlap += 1
