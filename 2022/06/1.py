21b331e64181b054f5fb48f4d5c384394294e072
Adil Naqvi <notadilnaqvi@gmail.com>
2022-12-08 00:13:49 +0500

    06: you have a message

---
+++ 2022/06/1.py
@@ -0,0 +1,27 @@
+def read_from_file(file):
+    data = None
+    with open(file) as f:
+        data = f.read()
+    return data
+
+CHARACTERS_IN_START_OF_PACKET_MARKER = 4
+
+def check_for_duplicates(sequence):
+    # If after removing (possible) duplicates, the length of the sequence changes,
+    # then that would mean that there were duplicates (that got removed).
+    return len(set(sequence)) != CHARACTERS_IN_START_OF_PACKET_MARKER
+
+def solve(file):
+    datastream = read_from_file(file)
+    for index, _ in enumerate(datastream):
+        has_duplicates = check_for_duplicates(datastream[index:index + CHARACTERS_IN_START_OF_PACKET_MARKER])
+        if not has_duplicates:
+            print(f'[{file}]: {index + CHARACTERS_IN_START_OF_PACKET_MARKER}')
+            break
+
+
+solve('sample-1.txt')
+solve('sample-2.txt')
+solve('sample-3.txt')
+solve('sample-4.txt')
+solve('input.txt')
